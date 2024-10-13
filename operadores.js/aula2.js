a70d5eeb86146418f6e6b2266983e74f837f558f
joaosouza2346 <jp8046@outlook.com>
2024-10-13 18:40:52 -0300

    first commit

---
+++ operadores.js/aula2.js
@@ -0,0 +1,18 @@
+var idade = 14 ;
+var nacionalidade = "americano";
+var idadeMinimaParaCNH = 15;
+ if(nacionalidade== "americano"){
+    idadeMinimaParaCNH = 12;
+    if (idade>= MinimaParaCNH) {
+        console.log ("pode tirar a CNH");
+
+    }
+    
+    
+ 
+     
+    
+}else {
+   console. log ("volte ano que vem");
+ }
+
